146b58f9bbc9b1d71d0cf95887dfa34872e9618c
hzyuan <hzyuan@corp.netease.com>
2025-10-15 11:20:53 +0800

    iOS 删除多余代码

--- NLiteAVDemo-Flutter/example/ios/Runner/AppDelegate.swift
+++ NLiteAVDemo-Flutter/example/ios/Runner/AppDelegate.swift
@@ -5,12 +5,10 @@
 import UIKit
 import Flutter
 import NIMSDK
-import NERtcCallKit
-import PushKit
 import AVFoundation
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, PKPushRegistryDelegate {
+@objc class AppDelegate: FlutterAppDelegate{
     
   override func application(
     _ application: UIApplication,
@@ -18,9 +16,6 @@ import AVFoundation
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     registerAPNS()
-//      if #available(iOS 17.4, *) {
-//          registerPushKit()
-//      }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     
@@ -49,51 +44,6 @@ import AVFoundation
     UIApplication.shared.registerForRemoteNotifications()
     UIApplication.shared.applicationIconBadgeNumber = 0
   }
-    
-  func registerPushKit() {
-    let pushRegistry = PKPushRegistry(queue: DispatchQueue.global(qos: .default))
-    pushRegistry.delegate = self
-    pushRegistry.desiredPushTypes = [.voIP]
-  }
-
-  // MARK: - PKPushRegistryDelegate
-    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
-        if pushCredentials.token.isEmpty {
-            print("voip token isEmpty")
-            return
-        }
-        
-        NIMSDK.shared().updatePushKitToken(pushCredentials.token)
-    }
-    
-    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
-        // 判断是否是云信发的payload
-        if payload.dictionaryPayload["nim"] == nil {
-            print("not found nim payload")
-            return
-        }
-        
-        let param = NECallSystemIncomingCallParam()
-        param.payload = payload.dictionaryPayload
-        
-        if #available(iOS 17.4, *) {
-            NECallEngine.sharedInstance().reportIncomingCall(with: param) { error, callInfo in
-                if let err = error {
-                    print("lck accept failed \(err.localizedDescription)")
-                }
-            } hangupCompletion: { error in
-                if let err = error {
-                    print("lck hangup error \(err.localizedDescription)")
-                }
-            } muteCompletion: { error, mute in
-                if let err = error {
-                    print("lck mute error \(err.localizedDescription)")
-                }
-            }
-        }
-
-        completion();
-    }
 }
 
 
